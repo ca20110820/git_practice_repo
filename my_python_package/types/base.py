0f0cf8e92a3da4b782eabc3f4dd1beeb5a2d1e07
ca20110820 <ca20110820@gmail.com>
2023-08-04 13:10:14 +0800

    Added some ABC and ABCMeta in base.py

--- my_python_package/types/base.py
+++ my_python_package/types/base.py
@@ -1,1 +1,11 @@
 # We will create our own types in this module.
+from abc import ABC, ABCMeta, abstractmethod
+
+
+class SomeABC(ABC):
+    @abstractmethod
+    def some_abstractmethod(self):
+        pass
+
+class A(metaclass=ABCMeta):
+    pass
